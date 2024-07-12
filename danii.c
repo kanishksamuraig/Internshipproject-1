1be199f9f3137a6b1c6830fa43772ae249b43fe0
kanishksamuraig <156745281+kanishksamuraig@users.noreply.github.com>
2024-07-12 15:17:11 +0530

    update danii,c

--- danii.c
+++ danii.c
@@ -1,28 +1,33 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-    printf("Hello world\n");
     int a,b;
     char c;
-    printf("Enter the first number");
+    printf("Enter the first number:");
     scanf("%d",&a);
-    printf("Enter the second number");
+    printf("Enter the second number:");
     scanf("%d",&b);
-    printf("Enter the operation you want to perform");
-    scanf("%c",&c);
-    if (c=='+')
+    printf("Enter the operation you want to perform:");
+    scanf(" %c",&c);
+    switch (c)
     {
-        printf("\n%d+%d=%d",a,b,a+b);
-    }
-    else if (c=='-')
-    {
-        printf("\n%d-%d=%d",a,b,a-b);
-    }
-    else if (c=='/')
-    {
-        printf("\n%d/%d=%d",a,b,a/b);
+        case '+':
+            printf("%d + %d = %d",a,b,a+b);
+            break;
+        case '-':
+            printf("%d - %d = %d",a,b,a-b);
+            break;
+        case '/':
+            printf("%d / %d = %d",a,b,a/b);
+            break;
+        case '*':
+            printf("%d * %d =%d",a,b,a*b);
+            break;
+        default:
+            printf("Enter the valid operator");
+            break;
     }
     return 0;
 
 }
+
